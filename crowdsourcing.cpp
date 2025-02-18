9efdbee5fd30389eebdc1ec9dfb737cdd52cd243
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2025-02-18 21:49:12 +0000

    easy!

--- crowdsourcing.cpp
+++ crowdsourcing.cpp
@@ -13,6 +13,33 @@
 
 int main()
 {
-    // write your code here
+    std::vector<std::string> songname = {};
+    std::vector<std::string> songartist = {};
+    std::vector<std::string> songduration = {};
+    std::cout << "would you like to enter a song in to the database? (y/n)" << std::endl;
+    std::string answer;
+    std::cin >> answer;
+
+    while (answer != "n")
+    {
+        std::cout << std::endl << "Enter the following information in order: " << std::endl << "1. any song name" << std::endl << "2. the song's artist" << std::endl << "3. the song's duration IN SECONDS" << std::endl;
+    
+        std::string sName;
+        std::string sArtist;
+        int sDuration;
+        std::cin >> sName >> sArtist >> sDuration;
+    
+        songname.push_back(sName);
+        songartist.push_back(sArtist);
+        songduration.push_back(sDuration);
+
+        std::cout << "would you like to play again? (y/n)" << std::endl;
+        std::string response;
+        std::cin >> response;
+    }
+    std::cout << "bye!" << std::endl;
+   
+
+   
     return 0;
 }
